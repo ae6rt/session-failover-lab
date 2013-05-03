52e3457f060805f08d7987fbedc5fe71cb9f31dc
Mark Petrovic <mspetrovic@gmail.com>
2013-05-03 06:08:18 -0700

    add XStream to pom to serialize session

--- src/main/java/org/petrovic/TestFilter.java
+++ src/main/java/org/petrovic/TestFilter.java
@@ -1,13 +1,17 @@
 package org.petrovic;
 
+import com.thoughtworks.xstream.XStream;
+
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpServletResponseWrapper;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 public class TestFilter implements Filter {
@@ -23,7 +27,11 @@ public class TestFilter implements Filter {
     @Override
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
         HttpServletResponseWrapper wrapper = new HttpServletResponseWrapper((HttpServletResponse) resp);
-        wrapper.setHeader("X-session-size", "32");
+        HttpServletRequest httpServletRequest = (HttpServletRequest) req;
+        HttpSession session = httpServletRequest.getSession();
+        String xml = new XStream().toXML(session);
+        wrapper.setHeader("X-session-size", Integer.toString(xml.length()));
+        System.out.printf("session: %s\n", xml);
         chain.doFilter(req, wrapper);
     }
 
